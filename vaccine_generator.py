6a320bcec97dbb48902e2a657e3a0a7c0c751315
Gabriel Whatley <whatley_gabriel@students.columbusstate.edu>
2024-02-28 16:44:55 -0500

    Made some changes for easier code readability and added more comments.

--- vaccine_generator.py
+++ vaccine_generator.py
@@ -4,10 +4,10 @@ import pymongo
 import datetime
 
 client = pymongo.MongoClient("mongodb+srv://vaccine_buddy_user:Br4bODkOhkhvcOU0@cluster0.2efzlbn.mongodb.net/?retryWrites=true&w=majority")
-mydb = client["vaccine_buddy"]  # Name of the database
-mycol = mydb["inventory"]  # Name of the collection
+mydb = client["vaccine_buddy"]  # Name of the database.
+mycol = mydb["inventory"]  # Name of the collection.
 
-month_end_dict = {
+month_end_dict = {  # Dictionary to tell how many days are in each month in 2024.
 	1: 31,
 	2: 29,
 	3: 31,
@@ -23,19 +23,23 @@ month_end_dict = {
 }
 
 
-def random_letters(length: int) -> string:  # Generate a string of random uppercase letters with user defined length
-	random_letters_id = ''.join(random.choice(string.ascii_uppercase) for i in range(length))
+def random_letters(length: int) -> string:  # Generate a string of random uppercase letters with user defined length.
+	random_letters_id = ""  # Create string to store the result
+	for i in range(length):  # Loop as many times as specified by argument.
+		random_letters_id += random.choice(string.ascii_uppercase)  # Append a random letter to the end of the string.
 	return random_letters_id
 
 
-def random_numbers(length: int) -> string:  # Generate a string of random numbers with user defined length
-	random_numbers_id = ''.join(random.choice(string.digits) for i in range(length))
+def random_numbers(length: int) -> string:  # Generate a string of random numbers with user defined length.
+	random_numbers_id = ""  # Create string to store the result
+	for i in range(length):  # Loop as many times as specified by argument.
+		random_numbers_id += random.choice(string.digits)  # Append a random letter to the end of the string.
 	return random_numbers_id
 
 
 def generate_vaccines(amount: int):  # Generate random vaccine test data and add it to the database.
 	for i in range(0, amount):  # Loop number of times specified by user.
-		manu_num = round(random.randrange(1,5))  # Generate a random manufacturer number 1 through 5
+		manu_num = round(random.randrange(1,5))  # Generate a random manufacturer number 1 through 5.
 		match manu_num:  # Based on the randomly generated manufacturer number, generate a random lot number and assign the manufacturer name.
 			case 1:
 				manu_name = "Astra Zeneca"
@@ -52,8 +56,12 @@ def generate_vaccines(amount: int):  # Generate random vaccine test data and add
 			case 5:
 				manu_name = "Moderna"
 				lot_num = random_numbers(3) + "-" + random_letters(4) + "-" + random_letters(3)
-		rand_month = round(random.randrange(1, 12))  # Generate a random expiration month.
-		month_end = month_end_dict.get(rand_month)  # From the random month, look up the last day from month_end_dict
-		mydict = {"manufacturer": manu_name, "lotNum": lot_num, "expDate": datetime.datetime(year=2024, month=round(random.randrange(1, 12)), day=round(random.randrange(1, month_end)))}  # Generate the vaccine record using all previously generated data.
+		rand_month = random.randint(1, 12)  # Generate a random expiration month.
+		month_end = month_end_dict.get(rand_month)  # From the random month, look up the last day from month_end_dict.
+		mydict = {"manufacturer": manu_name, "lotNum": lot_num, "expDate": datetime.datetime(year=2024, month=rand_month, day=random.randint(1, month_end))}  # Generate the vaccine record using all previously generated data.
 		result = mycol.insert_one(mydict)  # Add the vaccine record to the database.
-		print(result)
+		print(result) #  Print the results of the action to the console.
+
+
+def main():
+	generate_vaccines(600)
